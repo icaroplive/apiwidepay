e3a3faf4a3ba88e284a261de85350f0d0b4301ae
Icaro Pinheiro Rodrigues <icaro.rodrigues@fip-moc.edu.br>
2020-05-09 07:42:01 -0300

    gitignore

---
+++ Controllers/PdfController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using api_widepay.Interfaces;
+using api_widepay.Models.Retorno;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_widepay.Controllers {
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class PdfController : ControllerBase {
+
+        private IWidePay _wide;
+        public PdfController (IWidePay wide) {
+            _wide = wide;
+        }
+
+        [HttpGet ("{filename}")]
+        public IActionResult Get (string filename) {
+            const string contentType = "application/pdf";
+            HttpContext.Response.ContentType = contentType;
+            var result = new FileContentResult (System.IO.File.ReadAllBytes (@"boletos//" + filename), contentType) {
+                FileDownloadName = $"{filename}"
+            };
+
+            return result;
+        }
+
+        [HttpPost]
+        public string Post ([FromBody] List<int> idfin_movimento) {
+            var file = _wide.geraPDF (idfin_movimento).Result;
+            return file;
+        }
+    }
+}
---
+++ Models/Retorno/RetornoGerarPDF.cs
@@ -0,0 +1,9 @@
+namespace api_widepay.Models.Retorno
+{
+    public class RetornoGerarPDF
+    {
+        public bool sucesso { get; set; }
+        public int id { get; set; }
+        public string arquivo { get; set; }
+    }
+}
