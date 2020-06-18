74365e6eb76383c4afa5a93a20539b433594442e
Icaro Pinheiro Rodrigues <icaro.rodrigues@fip-moc.edu.br>
2020-06-18 16:59:20 -0300

    corrigindo datas venc e pag model financeiro

--- Models/Contas/fin_movimento.cs
+++ Models/Contas/fin_movimento.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace api_widepay.Models.Contas {
@@ -8,10 +9,11 @@ namespace api_widepay.Models.Contas {
 
         [ForeignKey ("cad_cliente")]
         public int idcad_descricao { get; set; }
-        public DateTime? data_pag { get; set; }
-        public DateTime data_venc { get; set; }
+
+        public DateTime data_pag { get; set; } = DateTime.Parse("01-01-0001");
+        public DateTime data_venc { get; set; } = DateTime.Parse("01-01-0001");
         public decimal vlr_cob { get; set; }
-        public DateTime? data_boleto { get; set; }
+        public DateTime data_boleto { get; set; } = DateTime.Parse("01-01-0001");
         public int status_pagamento { get; set; }
         public virtual cad_cliente cad_cliente { get; set; }
         public string idwidepay { get; set; }
