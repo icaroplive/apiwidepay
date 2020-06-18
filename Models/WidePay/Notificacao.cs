1e14ebbea077a2b0a54b5a822721ea035cee3752
Icaro Pinheiro Rodrigues <icaro.rodrigues@fip-moc.edu.br>
2020-06-18 17:01:40 -0300

    decimal para json null Notificação model widepay

--- Models/WidePay/Notificacao.cs
+++ Models/WidePay/Notificacao.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
-namespace api_widepay.Models.WidePay
-{
-    public class Notificacao
-    {
+namespace api_widepay.Models.WidePay {
+    public class Notificacao {
         public bool sucesso { get; set; }
         public cobranca cobranca { get; set; }
     }
     public class cobranca {
         public string id { get; set; }
-        public DateTime recebimento { get; set; }
+        public DateTime recebimento { get; set; } = DateTime.Parse("01-01-0001");
+
+        [JsonProperty (NullValueHandling = NullValueHandling.Ignore)]
+        public decimal recebido { get; set; }
+
+        [JsonProperty (NullValueHandling = NullValueHandling.Ignore)]
+        public decimal tarifa { get; set; }
         public string status { get; set; }
         public List<historico> historico { get; set; }
     }
