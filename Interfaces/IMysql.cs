442e6073f7a445b93df2c4f7e50ba583cfbd5507
Icaro Pinheiro Rodrigues <icaro.rodrigues@fip-moc.edu.br>
2020-06-18 16:59:52 -0300

    interface mysql

--- Interfaces/IMysql.cs
+++ Interfaces/IMysql.cs
@@ -1,15 +1,14 @@
+using System.Threading.Tasks;
 using api_widepay.Models.Contas;
 using api_widepay.Models.WidePay;
-using System.Threading.Tasks;
 
-namespace api_widepay.Interfaces
-{
-    public interface IMysql
-    {
-         fin_movimento buscarPorIdFinMovimento(int idfin_movimento);
-         cad_cliente buscarClientePorId(int idcad_cliente);
-         void atualizarFinMovimento (int idfin_movimento, string idwidepay, string codigo_barra);
-         fin_movimento baixarPagamento (Notificacao not);
+namespace api_widepay.Interfaces {
+    public interface IMysql {
+        fin_movimento buscarPorIdFinMovimento (int idfin_movimento);
+        cad_cliente buscarClientePorId (int idcad_cliente);
+        void atualizarFinMovimento (int idfin_movimento, string idwidepay, string codigo_barra);
+        fin_movimento baixarPagamento (Notificacao not);
+        
 
     }
 }
