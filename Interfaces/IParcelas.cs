2f0eb93943252bafaa4647f06abac4729128e75a
Icaro Pinheiro Rodrigues <icaro.rodrigues@fip-moc.edu.br>
2020-06-18 17:00:13 -0300

    remover parcelas sem cancelar IParlceas

--- Interfaces/IParcelas.cs
+++ Interfaces/IParcelas.cs
@@ -6,6 +6,8 @@ using api_widepay.Models.Retorno;
 namespace api_widepay.Interfaces {
     public interface IParcelas {
         List<fin_movimento> pegarParcelas (int idcad_descricao);
-        RetornoCancela removerParcelas (List<int> parcelas);
+        RetornoCancela cancelarEremoverParcelas (List<int> parcelas);
+
+        bool removerParcelas (List<int> parcelas);
     }
 }
