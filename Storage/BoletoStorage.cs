3844202947fca7d92d01b244852cd7c16cf262a5
Icaro Pinheiro Rodrigues <icaro.rodrigues@fip-moc.edu.br>
2021-01-03 02:12:07 -0300

    corrigindo diretorio de boletos api.ipr.net.br para ipr.net.br

--- Storage/BoletoStorage.cs
+++ Storage/BoletoStorage.cs
@@ -4,7 +4,7 @@ using api_widepay.Interfaces;
 namespace api_widepay.Storage {
     public class BoletoStorage : IBoletoStorage {
         public void gravarTxt (int idfin_movimento, string codigo) {
-            string filePath = @"/var/www/api.ipr.net.br/segunda-via/boletos/" + idfin_movimento + ".txt";
+            string filePath = @"/var/www/ipr.net.br/segunda-via/boletos/" + idfin_movimento + ".txt";
             
             using (StreamWriter outputFile = new StreamWriter(File.Open(filePath, System.IO.FileMode.Append))) {
                 outputFile.WriteLine (codigo.Replace("CPF: 095.441.926-03","CNPJ: 23.418.622/0001-30"));
