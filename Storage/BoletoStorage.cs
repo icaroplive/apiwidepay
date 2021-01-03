219adebf9b51921d80e9e2134652f739a7a39f8f
Icaro Pinheiro Rodrigues <icaro.rodrigues@fip-moc.edu.br>
2021-01-03 02:13:49 -0300

    removendo append mode na gravacao de boletos

--- Storage/BoletoStorage.cs
+++ Storage/BoletoStorage.cs
@@ -5,8 +5,7 @@ namespace api_widepay.Storage {
     public class BoletoStorage : IBoletoStorage {
         public void gravarTxt (int idfin_movimento, string codigo) {
             string filePath = @"/var/www/ipr.net.br/segunda-via/boletos/" + idfin_movimento + ".txt";
-            
-            using (StreamWriter outputFile = new StreamWriter(File.Open(filePath, System.IO.FileMode.Append))) {
+            using (StreamWriter outputFile = new StreamWriter (filePath)) {
                 outputFile.WriteLine (codigo.Replace("CPF: 095.441.926-03","CNPJ: 23.418.622/0001-30"));
             }
         }
