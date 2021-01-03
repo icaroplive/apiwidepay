c7b0b67e5625a9478e5775d98cc216758d701d78
Icaro Pinheiro Rodrigues <icaro.rodrigues@fip-moc.edu.br>
2021-01-03 02:09:50 -0300

    Streamwriter append mod

--- Storage/BoletoStorage.cs
+++ Storage/BoletoStorage.cs
@@ -5,7 +5,8 @@ namespace api_widepay.Storage {
     public class BoletoStorage : IBoletoStorage {
         public void gravarTxt (int idfin_movimento, string codigo) {
             string filePath = @"/var/www/api.ipr.net.br/segunda-via/boletos/" + idfin_movimento + ".txt";
-            using (StreamWriter outputFile = new StreamWriter (filePath)) {
+            
+            using (StreamWriter outputFile = new StreamWriter(File.Open(filePath, System.IO.FileMode.Append))) {
                 outputFile.WriteLine (codigo.Replace("CPF: 095.441.926-03","CNPJ: 23.418.622/0001-30"));
             }
         }
